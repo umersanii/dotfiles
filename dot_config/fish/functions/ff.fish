99f179aea7cce8ae6d35f2ce88a09e97a41e7fd6
sani <iamumersani@gmail.com>
2026-07-03 14:00:41 +0500

    Update .config/fish/functions/ff.fish

--- dot_config/fish/functions/ff.fish
+++ dot_config/fish/functions/ff.fish
@@ -1,7 +1,10 @@
 function ff
     clear
     # Play GIF in the background at top-left (Increased size)
-    kitty +kitten icat --loop=0 --align=left --place 50x34@0x6 /home/sani/Downloads/touch.jpg
-    printf '\033[H'  # reset cursor to top-left before fastfetch
-    fastfetch --logo " " --logo-type data-raw --logo-width 50 --logo-height 30 --logo-padding-left 00
+    kitty +kitten icat --loop=0 --align=left --place 50x34@0x2 /home/sani/Downloads/touch.jpg
+
+    # Run fastfetch with a "ghost logo" (single space) to force padding
+    # --logo " " + --logo-type data-raw makes it think there's a logo
+    # --logo-width and --logo-padding-left push the text to the right (Adjusted for larger GIF)
+    fastfetch --logo " " --logo-type data-raw --logo-width 50 --logo-height 30 --logo-padding-left 00 --logo-padding-top 0
 end
